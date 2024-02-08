bb8a25fa34f7e34bf8ea998d8eccf9f1926bb30c
PeterLenon <goshomip@gmail.com>
2024-02-08 00:43:16 -0600

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+function showLoveMessage() {
+  document.getElementById("loveMessage").innerHTML = "Message me 3 hearts";
+  document.getElementById("loveMessage").style.display = "block";
+}
+function showBribeMessage() {
+  document.getElementById("loveMessage").innerHTML = "How about a bribe?";
+  document.getElementById("loveMessage").style.display = "block";
+}
